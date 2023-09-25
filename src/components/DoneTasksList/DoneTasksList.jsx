5af1fa47f732dcecfd6abe381793b7d4c28a1eb4
Alexander Gurinovich <hrumstik.warsaw@gmail.com>
2023-09-25 11:24:30 +0200

    Enhanced code styling, removed CSS styles unrelated to CSSTransition, and added destructuring.

--- src/components/DoneTasksList/DoneTasksList.jsx
+++ src/components/DoneTasksList/DoneTasksList.jsx
@@ -1,63 +1,72 @@
-import './DoneTasksList.css';
+import "./DoneTasksList.css";
 import TaskListItem from "../TaskListItem/TaskListItem";
 import { useState, useEffect, useCallback } from "react";
-import { useSelector } from 'react-redux';
-import { CSSTransition } from 'react-transition-group';
+import { useSelector } from "react-redux";
+import { CSSTransition } from "react-transition-group";
+import styled from "styled-components";
 import { Box, IconButton, Typography } from "@mui/material";
-import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function DoneTasksList({ tasksArray }) {
-    const [visibilityOfDoneTasks, setVisibilityOfDoneTasks] = useState(false);
-    const [doneTasksCount, setDoneTasksCount] = useState(null)
-    const tasks = useSelector((state) => state.tasks.tasks);
+  const [visibilityOfDoneTasks, setVisibilityOfDoneTasks] = useState(false);
+  const [doneTasksCount, setDoneTasksCount] = useState(null);
+  const tasks = useSelector(({ tasks }) => tasks.tasks);
 
-    const countDoneTasks = useCallback(() => {
-        const doneTasksArray = tasksArray.filter(task => task.done === true)
-        setDoneTasksCount(doneTasksArray.length);
-    }, [tasksArray]);
+  const countDoneTasks = useCallback(() => {
+    const doneTasksArray = tasksArray.filter((task) => task.done === true);
+    setDoneTasksCount(doneTasksArray.length);
+  }, [tasksArray]);
 
-    useEffect(() => {
-        countDoneTasks()
-    }, [tasks, countDoneTasks]);
+  const StyledCompletedBox = styled(Box)`
+    display: flex;
+    gap: 22px;
+  `;
 
-    return (
-        <>
-            <Box
-                onClick={() => { setVisibilityOfDoneTasks(!visibilityOfDoneTasks) }}
-                sx={{ display: 'flex', gap: '22px' }}>
-                <Typography sx={{ cursor: 'pointer', fontWeight: 'bold', color: "text.primary" }}>
-                    <IconButton>
-                        {visibilityOfDoneTasks
-                            ? <KeyboardArrowDownIcon
-                                sx={{ color: "icons.primary" }} />
-                            : <KeyboardArrowRightOutlinedIcon
-                                sx={{ color: "icons.primary" }} />}
-                    </IconButton>
-                    Completed
-                </Typography>
-                <Typography
-                    sx={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', color: "text.primary" }}
-                    variant="subtitle1">
-                    {doneTasksCount ? doneTasksCount : null}
-                </Typography>
-            </Box>
-            <CSSTransition
-                in={visibilityOfDoneTasks && tasks.length}
-                timeout={300}
-                classNames='done-tasks'
-                unmountOnExit>
-                <Box>
-                    {tasksArray.map((task, i) => {
-                        if (task.done === true) {
-                            return (<TaskListItem text={task.name} checked={task.done}
-                                key={i} />)
-                        } else {
-                            return null
-                        }
-                    })}
-                </Box>
-            </CSSTransition>
-        </>
-    )
+  const StyledTitleOfDoneTasks = styled(Typography)`
+    cursor: pointer;
+    font-weight: bold;
+  `;
+
+  useEffect(() => {
+    countDoneTasks();
+  }, [tasks, countDoneTasks]);
+
+  return (
+    <>
+      <StyledCompletedBox
+        onClick={() => {
+          setVisibilityOfDoneTasks(!visibilityOfDoneTasks);
+        }}
+      >
+        <StyledTitleOfDoneTasks color="text.primary">
+          <IconButton>
+            {visibilityOfDoneTasks ? (
+              <KeyboardArrowDownIcon color="icons.primary" />
+            ) : (
+              <KeyboardArrowRightOutlinedIcon color="icons.primary" />
+            )}
+          </IconButton>
+          Completed {doneTasksCount ? doneTasksCount : null}
+        </StyledTitleOfDoneTasks>
+      </StyledCompletedBox>
+
+      <CSSTransition
+        in={visibilityOfDoneTasks && tasks.length}
+        timeout={300}
+        classNames="done-tasks"
+        unmountOnExit
+      >
+        <Box>
+          {tasksArray.map(({ name, done, id }) => {
+            if (done === true) {
+              return <TaskListItem text={name} checked={done} key={id} />;
+            } else {
+              return null;
+            }
+          })}
+        </Box>
+      </CSSTransition>
+    </>
+  );
 }
